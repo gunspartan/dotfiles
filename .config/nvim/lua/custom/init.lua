8712c3b6ef07acbaa35d56eb59d22c2a90668b21
sunaboi <sunaboi@sunaboi.xyz>
2025-02-20 17:31:38 -0800

    add nvim configs

---
+++ .config/nvim/lua/custom/init.lua
@@ -0,0 +1,15 @@
+vim.opt.colorcolumn = "80"
+
+vim.opt.nu = true
+vim.opt.rnu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.wrap = false
+
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
---
+++ .config/nvim/lua/custom/mappings.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.general = {
+  n = {
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+  }
+}
+
+M.telescope = {
+  n = {
+    ["<C-p>"] = {"<cmd> Telescope git_files <CR>", "Find Git Files"},
+  }
+}
+return M
