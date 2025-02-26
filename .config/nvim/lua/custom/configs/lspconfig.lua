e0e1ade4aa7fae10f1110d76cb08aab32c11e7ca
sunaboi <sunaboi@sunaboi.xyz>
2025-02-25 20:25:54 -0800

    update nvim configs

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -9,3 +9,49 @@ lspconfig.rust_analyzer.setup({
   filetypes = {"rust"},
   root_dir = lspconfig.util.root_pattern("Cargo.toml"),
 })
+
+lspconfig.html.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+lspconfig.ts_ls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+lspconfig.cssls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+lspconfig.emmet_ls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte", "javascript", "typescript" },
+})
+
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+-- configure lua server (with special settigns)
+lspconfig.lua_ls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      -- make the language server recognize "vim" global
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
+        },
+      },
+    },
+  }
+})
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -12,6 +12,37 @@ local plugins = {
   },
   {
     "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim"
+    },
+    config = function()
+        -- import mason
+        local mason = require("mason")
+
+        -- import mason-lspconfig
+        local mason_lspconfig = require("mason-lspconfig")
+
+        -- enable mason and configure icons
+        mason.setup({
+            mason_lspconfig.setup({
+                -- list of servers for mason to install
+                ensure_installed = {
+                    "ts_ls",
+                    "html",
+                    "cssls",
+                    "tailwindcss",
+                    "svelte",
+                    "lua_ls",
+                    "graphql",
+                    "emmet_ls",
+                    "prismals",
+                    "pyright",
+                },
+            -- auto-install configured servers (with lspconfig)
+            automatic_installation = true, -- not the same as ensure_installed
+            }),
+        })
+    end,
     opts = {
       ensure_installed = {
         "rust-analyzer",
